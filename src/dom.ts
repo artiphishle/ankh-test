6fd15a836ea8abc9150bbc74ae9341389b23c345
artiphishle <fabio.gartenmann@protonmail.com>
2025-04-20 05:10:54 +0200

    Let vitest handle jsdom

--- src/dom.ts
+++
@@ -1,9 +0,0 @@
-import { JSDOM } from 'jsdom'
-
-// Nur initialisieren, wenn jsdom nicht schon da ist
-if (typeof window === 'undefined' || !window.document) {
-  const dom = new JSDOM('', { url: 'http://localhost' })
-  globalThis.window = dom.window as unknown as typeof globalThis.window
-  globalThis.document = dom.window.document
-  globalThis.navigator = dom.window.navigator
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export * from './dom'
-
 // render, screen
 export * from './react'
 
