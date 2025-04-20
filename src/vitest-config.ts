722f20f89472bb20f784280be29e2fb309bb1256
artiphishle <fabio.gartenmann@protonmail.com>
2025-04-20 05:51:09 +0200

    No comment

--- src/vitest-config.ts
+++ src/vitest-config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath } from 'url'
+
+// 👇 Resolve your own dist file from inside ankh-test
+const setupFile = fileURLToPath(
+  new URL('./vitest-setup.js', import.meta.url)
+)
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environment: 'node',
-    setupFiles: ['ankh-test/dist/vitest-setup.js']
-
+    setupFiles: [setupFile]
   }
 })
